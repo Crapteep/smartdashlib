cf3b16d7f8cb81d0599c7f571a48ca64667317a8
Crapteep <crapteep@gmail.com>
2024-07-23 00:52:10 +0200

    A simple example has been added to show the idea and principle of operation

---
+++ examples/basic.py
@@ -0,0 +1,35 @@
+from smartdashlib import SmartDash
+import random
+import asyncio
+
+"""use the on_data decorator, which calls the following function after receiving data on the used pin, or simply send data in a while loop
+"""
+async def main():
+    TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXZpY2VfaWQiOiI2NjllZDBmOTNjNWMxNmM0ZDQwZjY4ZjciLCJ0eXBlIjoicnBpIiwiZXhwIjoxNzUzMjIwMjE3fQ.7DrAfleeo5_cnT4wUymGaXwVY5gy_lq7b8fzHKK7QOg"
+
+    smart_dash = SmartDash(token=TOKEN)
+    await smart_dash.run()
+
+
+    @smart_dash.on_data("V100")
+    async def random_number(val):
+        await smart_dash.write("V1", random.randint(0,10))
+    
+
+    @smart_dash.on_data("V3")
+    async def toggle_trigger(val):
+        if val == 0:
+            await smart_dash.switch_trigger("V100", False)
+        else:
+            await smart_dash.switch_trigger("V100", True)
+
+    
+
+    while True:
+        # await smart_dash.write('V2', random.randint(10,20))
+        await asyncio.sleep(0.1) #this delay is needed for now
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
